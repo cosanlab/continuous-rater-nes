889b5546638b884fb1a3a893d1364f6f818c59b7
JonathanLim-19 <jonathanlim@Jonathans-MacBook-Pro-2.local>
2022-03-02 17:48:39 -0600

    initialize repository

---
+++ src/utils.js
@@ -0,0 +1,104 @@
+
+// ************************************************
+// THIS PAGE REQUIRES EXPERIMENTER INPUT
+// ************************************************
+
+// don't change these import statements
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/performance";
+import "firebase/analytics";
+import { writable } from 'svelte/store';
+
+// ************************************************
+// ************************************************
+// ************************************************
+// ************************************************
+// USER VARIABLES (FILL STUFF IN BELOW THIS LINE)
+// ************************************************
+// ************************************************
+// ************************************************
+// ************************************************
+
+// lab variables
+export const studyLocation = 'Hanover, NH'; // location of lab running mturk study
+export const labName = 'CosanLab'; // name of lab running HIT experiment 
+export const email = 'jonathan@twinbamboo.com'; // lab email for mturk
+export const studyAim = 'track emotional variance across different media'; // aim of mturk study 
+export const studyTasks = 'record variance of a specific emotion in a video'; // brief summary of HIT task for consent form
+export const experiment = 'Emotion'; // name of experiment (should match collection name in firebase)
+
+// HIT variables
+export const HITPay = '2.50'; // pay for HIT completion (format as X.XX with no dollar sign)
+export const userGroup = 'Demo'; // name of collection of participants for current HIT
+export const estHITTime = '5'; // estimated time to complete HIT (in minutes)
+export const totalHITTime = estHITTime * 2; // total time provided for HIT (in minutes)
+
+// stimuli variables      
+export const ratingTypes = ['afraid', 'amused', 'angry', 'anxious', 'bored', 'disgusted', 'frusturated', 'happy', 'inspired', 'moved', 'proud', 'relieved', 'sad', 'surprised', 'uncomfortable', 'calm', 'hungry', 'awed', 'romantic']; // array of rating types   
+
+// this configures path to proper firebase
+// COPY AND PASTE YOUR FIREBASE CONFIG HERE
+let firebaseConfig = {
+     apiKey: "AIzaSyDuJK7KS7LynZZQ0CtZY5BYTcP7JW9rUOI",
+     authDomain: "emotion-rating-system.firebaseapp.com",
+     projectId: "emotion-rating-system",
+     storageBucket: "emotion-rating-system.appspot.com",
+     messagingSenderId: "709565333316",
+     appId: "1:709565333316:web:e8e5610456c4d8015de111",
+     measurementId: "G-EMRGK8DYVT"
+    };
+
+// ************************************************
+// ************************************************
+// ************************************************
+// ************************************************
+// STOP. DON'T CHANGE ANYTHING BELOW THIS LINE
+// ************************************************
+// ************************************************
+// ************************************************
+// ************************************************
+
+// dev is referenced as a store elsewhere in the code, so cannot be a simple Boolean
+// eslint-disable-next-line no-undef
+export const dev = DEV_MODE ? writable(true) : writable(false);
+
+// firebase info (export for use elsewhere in app)
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const serverTime = firebase.firestore.Timestamp.now();
+
+// Functions to parse the URL to get workerID, hitID, and assignmentID
+const unescapeURL = (s) => decodeURIComponent(s.replace(/\+/g, '%20'));
+export const getURLParams = () => {
+    const params = {};
+    let url = window.location.href;
+    let m = window.location.href.match(/[\\?&]([^=]+)=([^&#]*)/g);
+    
+    if (m) {
+        let i = 0;
+        while (i < m.length) {
+            const a = m[i].match(/.([^=]+)=(.*)/);
+            params[unescapeURL(a[1])] = unescapeURL(a[2]);
+            i += 1;
+        }
+    }
+    if (!params.workerId && !params.assignmentId && !params.hitId) {
+        // eslint-disable-next-line no-undef
+        if (DEV_MODE) {
+            console.log(
+                'App running in dev mode so HIT submission will not work!\nTo test in the sandbox make sure to deploy the app.'
+            );
+            params.workerId = 'test-worker';
+            params.assignmentId = 'test-assignment';
+            params.hitId = 'test-hit';
+            params.turkSubmitTo = 'test-submit';
+        }
+    }
+    return params;
+};
+
+// Use those functions to get the window URL params and make them available throughout the app
+export const params = getURLParams();
